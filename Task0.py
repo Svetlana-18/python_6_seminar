e4fa515ed1a454dc97b788a37dab76bee4431ac6
Svetlana-18 <vasilevichsvetlana@yandex.ru>
2022-10-16 22:10:01 +0300

    создала черновики

--- Task0.py
+++ Task0.py
@@ -13,3 +13,29 @@
 # out
 # [28, 20, 10, 5, 1, 24, 7, 15, 23, 25]
 # [24, 15, 23, 25]
+
+
+from random import sample
+
+
+def more_then(num):
+    my_list = sample(range(num * 3), num)
+    print(my_list)
+    return [my_list[num] for num in range(1, len(my_list)) if my_list[num] > my_list[num - 1]]
+
+
+print(more_then(int(input())))
+
+#  ------------------------------------------- вариант решения ---------------------------------------------------------
+
+from random import randint
+
+
+def more_then(num):
+    original_list = [randint(0, 1000) for _ in range(num)]
+    print(original_list)
+    return [num for i, num in enumerate(original_list[1:]) if num > original_list[i]]
+
+
+print(more_then(int(input())))
+
--- Task4.py
+++ Task4.py
@@ -16,3 +16,35 @@
 # ['автомобиль ночью мягкий', 'огонь вчера веселый', 'автомобиль позавчера веселый', 'город вчера утопичный', 
 # 'лес сегодня зеленый', 'дом вчера яркий', 'автомобиль вчера зеленый', 'огонь позавчера яркий', 'огонь где-то утопичный',
 #  'автомобиль где-то мягкий']
+
+
+from random import choice, randrange
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью", "когда-то", "где-то"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+
+def some_jokes(n: int, repeat: bool = False) -> list:
+    """
+    Функция возвращает случайные шутки, собранные из трех списков слов
+
+    :param n: количество шуток
+    :param repeat: уникальные/неуникальные
+    :return: список случайных шуток
+
+    """
+    no, adv, adj = nouns.copy(), adverbs.copy(), adjectives.copy()
+    list_of_j = []
+    list_min = min(no, adv, adj)
+
+    for i in range(len(list_min) % n if repeat else n):
+        num = randrange(len(list_min))
+        list_of_j.append(f"{no.pop(num)} {adv.pop(num)} {adj.pop(num)}" if repeat
+                         else f"{choice(nouns)} {choice(adverbs)} {choice(adjectives)}")
+    return list_of_j
+
+
+print(some_jokes(100, True))
+print(help(some_jokes))
+print(some_jokes.__doc__)
